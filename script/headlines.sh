b0b0eb7b22d8e9fca0c2265d281943355386c61a
Daniel Westermann-Clark <dwc@ufl.edu>
2009-11-16 21:13:28 +0000

    Set umask explicitly since something seems to have changed on glint

--- script/headlines.sh
+++ script/headlines.sh
@@ -5,12 +5,13 @@ file="$2"
 number="$3"
 template="$4"
 
-if [ "$template" == "" ]
-then template="../root/www.ufl.edu/headlines.html.tmpl"
+if [ "x$template" == "x" ]; then
+    template="../root/www.ufl.edu/headlines.html.tmpl"
 fi
 
 log="$HOME"/log/headlines.txt
 
+umask 022
 cd "$(dirname $0)" &&
     ./headlines.pl "$feed" "$template" "$number" > "$file.tmp" 2>> "$log" &&
     mv "$file.tmp" "$file"
